8e932cf549a2a62cdf5c03b18ed2268a28469c27
Harlan Howe <harlan.howe@kinkaid.org>
2024-10-06 11:52:50 -0500

    ItemPanel initial commit

--- src/ItemPanel.java
+++ src/ItemPanel.java
@@ -1,3 +1,21 @@
-public class ItemPanel
+import javax.swing.*;
+import java.util.List;
+
+public class ItemPanel extends JPanel
 {
+    private ListSubPanel detailList;
+    private Item currentItem;
+    private DetailPanel theDetailPanel;
+
+    public ItemPanel()
+    {
+        super();
+        detailList = new ListSubPanel();
+        currentItem = null;
+    }
+
+    public void setDetailPanel(DetailPanel dp)
+    {
+        theDetailPanel = dp;
+    }
 }
