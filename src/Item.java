1d1c148ba67b77c525a37006d4332edd1e73ae21
Harlan Howe <harlan.howe@kinkaid.org>
2024-10-06 12:32:34 -0500

    Instantiated and initialized variables for the list of Detail objects, a title and subtitle; wrote getDetails() accessor.

--- src/Item.java
+++ src/Item.java
@@ -1,3 +1,18 @@
 public class Item
 {
+    private Detail[] details;
+    private String title, subtitle;
+
+    public Item()
+    {
+        details = null;
+        title = "Generic";
+        subtitle = "No info yet.";
+    }
+
+    public Detail[] getDetails()
+    {
+        return details;
+    }
+
 }
