66e4dc467c1a52084f180a791d1a379c8dac0845
Harlan Howe <harlan.howe@kinkaid.org>
2024-10-06 11:49:20 -0500

    PrimaryListPanel initial commit

--- src/PrimaryListPanel.java
+++ src/PrimaryListPanel.java
@@ -1,3 +1,21 @@
-public class PrimaryListPanel
+import javax.swing.*;
+
+public class PrimaryListPanel extends JPanel
 {
+    private ListSubPanel listGUI;
+    private Item[] items;
+    private ItemPanel theItemPanel;
+
+
+    public PrimaryListPanel()
+    {
+        super();
+        listGUI = new ListSubPanel();
+        items = null;
+    }
+
+    public void setItemPanel(ItemPanel ip)
+    {
+        theItemPanel = ip;
+    }
 }
