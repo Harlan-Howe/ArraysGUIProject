16e193c9e25c81cb2a0d8b11ee67ea4c32bb42ad
Harlan Howe <harlan.howe@kinkaid.org>
2024-10-06 12:30:13 -0500

    implementing PanelManager. Currently dependent on Item's getDetails() method, which I haven't written.

--- src/ItemPanel.java
+++ src/ItemPanel.java
@@ -1,21 +1,51 @@
 import javax.swing.*;
 import java.util.List;
 
-public class ItemPanel extends JPanel
+public class ItemPanel extends JPanel implements PanelManager
 {
-    private ListSubPanel detailList;
+    private ListSubPanel<Detail> detailList;
     private Item currentItem;
     private DetailPanel theDetailPanel;
 
     public ItemPanel()
     {
         super();
-        detailList = new ListSubPanel();
+        detailList = new ListSubPanel<Detail>(this);
         currentItem = null;
     }
 
     public void setDetailPanel(DetailPanel dp)
     {
         theDetailPanel = dp;
+    }
+
+    @Override
+    public void handleShiftUp(int index)
+    {
+        System.out.println(STR."User would like to shift the Detail at \{index} up.");
+    }
+
+    @Override
+    public void handleShiftDown(int index)
+    {
+        System.out.println(STR."User would like to shift the Detail at \{index} down.");
+    }
+
+    @Override
+    public void handleAdd()
+    {
+        System.out.println("User would like to add another Detail.");
+    }
+
+    @Override
+    public void handleRemove(int index)
+    {
+        System.out.println(STR."User would like to remove the item at \{index}.");
+    }
+
+    @Override
+    public Object[] getListData()
+    {
+        return currentItem.getDetails();
     }
 }
