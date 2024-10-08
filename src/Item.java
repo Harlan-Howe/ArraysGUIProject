ec5b95143bea991f66204a4a3a080f6a9041f854
Harlan Howe <harlan.howe@kinkaid.org>
2024-10-08 11:05:38 -0500

    Item now has a name attribute.

--- src/Item.java
+++ src/Item.java
@@ -1,3 +1,20 @@
 public class Item
 {
+    private String name;
+
+    public Item()
+    {
+        name = STR."Item #\{(int)(1000*Math.random())}";
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    @Override
+    public String toString()
+    {
+        return name;
+    }
 }
