b5ad7e3dbfee9f8b7905c1336959798ff7d474fe
Harlan Howe <harlan.howe@kinkaid.org>
2024-10-08 13:55:46 -0500

    Started save request process, generating the string.

--- src/PrimaryListPanel.java
+++ src/PrimaryListPanel.java
@@ -1,8 +1,10 @@
 import javax.swing.*;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class PrimaryListPanel extends JPanel implements ListSelectionListener
+public class PrimaryListPanel extends JPanel implements ListSelectionListener, ActionListener
 {
     private ListSubPanel<Category> listGUI;
     private CategoryCollection theCategoryCollection;
@@ -30,10 +32,29 @@ public class PrimaryListPanel extends JPanel implements ListSelectionListener
         theCategoryPanel = cp;
     }
 
+    public void handleSaveRequest()
+    {
+        System.out.println("Saving.");
+        StringBuilder builder = new StringBuilder();
+        int numCategories = theCategoryCollection.getListData().length;
+        builder.append(numCategories);
+        builder.append("\n");
+        for (int i=0; i<numCategories; i++)
+            builder.append(theCategoryCollection.getCategoryAtIndex(i).getSaveString());
+    }
+
     @Override
     public void valueChanged(ListSelectionEvent e)
     {
         System.out.println(STR."The user just changed the selection in the list to #\{e.getFirstIndex()}. I should probably do something about that.");
         theCategoryPanel.setCurrentCategory(theCategoryCollection.getCategoryAtIndex(e.getFirstIndex()));
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+        if (e.getActionCommand().equals("Save"))
+            handleSaveRequest();
+
     }
 }
