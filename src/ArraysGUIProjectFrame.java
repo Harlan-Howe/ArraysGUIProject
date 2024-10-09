d7047d066f60b4b12ed21f08bc0c2a9c6b00fbb0
Harlan Howe <harlan.howe@kinkaid.org>
2024-10-09 10:59:47 -0500

    Documentation

--- src/ArraysGUIProjectFrame.java
+++ src/ArraysGUIProjectFrame.java
@@ -3,12 +3,16 @@ import java.awt.*;
 import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
 
+/**
+ * This class sets up the overall window, dividing up the screen into the three panels and establishing the menus.
+ */
 public class ArraysGUIProjectFrame extends JFrame
 {
-
+    // variables for the three panels on the page.
     private PrimaryListPanel leftPanel;
     private CategoryPanel centerPanel;
     private ItemPanel rightPanel;
+    // menu setup variables
     private JMenuBar menuBar;
     private JMenu fileMenu;
     private JMenuItem newMI, openMI, saveMI, saveAsMI;
@@ -36,6 +40,7 @@ public class ArraysGUIProjectFrame extends JFrame
         this.getContentPane().add(centerPanel);
         this.getContentPane().add(rightPanel);
 
+        // tell the two left panes about the ones just to the right of them. - they have methods for this.
         leftPanel.setCategoryPanel(centerPanel);
         centerPanel.setItemPanel(rightPanel);
 
@@ -44,31 +49,34 @@ public class ArraysGUIProjectFrame extends JFrame
 
     public void makeMenus()
     {
+        // build the menubar and the File menu.
         menuBar = new JMenuBar();
         this.setJMenuBar(menuBar);
         fileMenu = new JMenu("File");
         menuBar.add(fileMenu);
 
+        // create the four items that will (eventually) go in the file menu
         newMI = new JMenuItem("New");
         openMI = new JMenuItem("Open...");
         saveMI = new JMenuItem("Save");
         saveAsMI = new JMenuItem("Save As...");
 
+        // add keyboard shortcuts... I got fancy.
         newMI.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, java.awt.event.InputEvent.META_DOWN_MASK));
         openMI.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, java.awt.event.InputEvent.META_DOWN_MASK));
         saveMI.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, java.awt.event.InputEvent.META_DOWN_MASK));
         saveAsMI.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, java.awt.event.InputEvent.META_DOWN_MASK+ InputEvent.SHIFT_DOWN_MASK));
 
+        // if the user selects one of these menu items, the left panel should be told about it.
         newMI.addActionListener(leftPanel);
         openMI.addActionListener(leftPanel);
         saveMI.addActionListener(leftPanel);
         saveAsMI.addActionListener(leftPanel);
 
+        // put the menu items into the File menu.
         fileMenu.add(newMI);
         fileMenu.add(openMI);
         fileMenu.add(saveMI);
         fileMenu.add(saveAsMI);
-
-
     }
 }
