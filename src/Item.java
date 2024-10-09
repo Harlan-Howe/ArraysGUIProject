246aa00e2de1de7026004c8d0c4be051d3b5f3de
Harlan Howe <harlan.howe@kinkaid.org>
2024-10-08 22:33:27 -0500

    Wrote overloaded constructor.

--- src/Item.java
+++ src/Item.java
@@ -7,6 +7,11 @@ public class Item
         name = STR."Item #\{(int)(1000*Math.random())}";
     }
 
+    public Item(String lineToDecode)
+    {
+        //TODO: parse the data in lineToDecode to fill in the variables for this class.
+    }
+
     public String getName()
     {
         return name;
